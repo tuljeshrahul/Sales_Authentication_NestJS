a2d31171102f281afa886a49af5edbf1b8073b20
unknown <tuljeshrahul@gmail.com>
2024-10-21 17:34:38 +0530

    first commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
@@ -12,6 +12,7 @@ export class AuthService {
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(email);
+    console.log(password);
     if (user && await bcrypt.compare(password, user.password)) {
       const { password, ...result } = user;
       return result;
@@ -27,6 +28,12 @@ export class AuthService {
   }
 
   async register(email: string, password: string, role: string) {
+    const existingUser = await this.usersService.findOne(email);
+    if (existingUser) {
+      throw new BadRequestException('email already exists');
+    }
+    password = await bcrypt.hash(password, 10);
+    console.log(password);
     return this.usersService.create(email, password, role);
   }
 }
